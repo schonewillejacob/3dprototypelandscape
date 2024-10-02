affbfb33f6879e76e39e1ec729f30953ed487bc2
Jacob <winkschonewille@gmail.com>
2024-10-02 11:08:11 -0700

    progress timer output

--- glider/glider.gd
+++ glider/glider.gd
@@ -8,10 +8,18 @@ extends Node3D
 @onready var node_GliderMesh = $GliderMesh
 
 var SPEED = 0.01
+var log_progess_timer = Timer.new()
+@export var debug_timer = true
 
 
 
 # VIRTUALS #####################################################################
+func _ready() -> void:
+	if debug_timer:
+		log_progess_timer.timeout.connect(_on_progress_timeout)
+		add_child(log_progess_timer)
+		log_progess_timer.start(1)
+
 func _process(delta: float) -> void:
 	push_partial_transform_to_camera()
 	target_path_follower.progress_ratio += delta * SPEED
@@ -23,3 +31,10 @@ func push_partial_transform_to_camera() -> void:
 	if camera_rig: 
 		camera_rig.transform.basis = transform.basis
 		camera_rig.global_position = global_position
+
+
+
+# SIGNALS ######################################################################
+func _on_progress_timeout() -> void:
+	print(target_path_follower.progress_ratio)
+	log_progess_timer.start(1)
